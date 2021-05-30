b260a8ff6b89e2160f52a2c4146f13197255326d
Tom Busby <tom@busby.ninja>
2021-05-30 14:41:27 +0200

    Do the ProducePrices value-correction exercises

--- src/DataCorrection.hs
+++ src/DataCorrection.hs
@@ -1,5 +1,7 @@
+{-# LANGUAGE NamedFieldPuns #-}
 module DataCorrection where
 
+import Prelude hiding (round)
 import Control.Lens
 
 data Time = Time
@@ -31,3 +33,42 @@ mins = lens getter setter
       -- We should probably be using setters to do this to avoid issues
       -- if we ever add fields, or turn existing ones into virtual fields
       = Time ((h + (newMinutes `div` 60)) `mod` 24) (newMinutes `mod` 60)
+
+data ProducePrices = ProducePrices
+  { _limePrice :: Float
+  , _lemonPrice :: Float
+  }
+  deriving Show
+
+round :: Float -> Float
+round = max 0.0
+
+limePrice :: Lens' ProducePrices Float
+limePrice = lens getter setter
+  where
+    getter = _limePrice
+    setter pp@(ProducePrices {_lemonPrice}) price =
+      let correctedPrice = round price
+          correctedLemonPrice =
+            if correctedPrice > _lemonPrice
+              then max _lemonPrice $ correctedPrice - 0.50
+              else min _lemonPrice $ correctedPrice + 0.50
+      in pp
+            { _limePrice = round correctedPrice
+            , _lemonPrice = correctedLemonPrice
+            }
+
+lemonPrice :: Lens' ProducePrices Float
+lemonPrice = lens getter setter
+  where
+    getter = _lemonPrice
+    setter pp@(ProducePrices {_limePrice}) price =
+      let correctedPrice = round price
+          correctedLimePrice =
+            if correctedPrice > _limePrice
+              then max _limePrice $ correctedPrice - 0.50
+              else min _limePrice $ correctedPrice + 0.50
+      in  pp
+            { _lemonPrice = round correctedPrice
+            , _limePrice = correctedLimePrice
+            }
