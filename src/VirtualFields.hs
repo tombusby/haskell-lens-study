b58c19debd98cd6f95a3951ed6f1243f399d1521
Tom Busby <tom@busby.ninja>
2021-05-30 13:27:32 +0200

    Switch Temperature type to Kelvin and provide a new celcius virtual field lens

--- src/VirtualFields.hs
+++ src/VirtualFields.hs
@@ -4,7 +4,7 @@ import Control.Lens
 
 data Temperature = Temperature
   { _location :: String
-  , _celsius :: Float
+  , _kelvin :: Float
   }
   deriving (Show)
 
@@ -16,8 +16,18 @@ celsiusToFahrenheit c = (c * (9/5)) + 32
 fahrenheitToCelsius :: Float -> Float
 fahrenheitToCelsius f = (f - 32) * (5/9)
 
+-- We originally built the Temperature type with a _celcius field and then
+-- provided this fahrenheit field
 fahrenheit :: Lens' Temperature Float
 fahrenheit = lens getter setter
   where
     getter = celsiusToFahrenheit . view celsius
     setter struct val = set celsius (fahrenheitToCelsius val) struct
+
+-- After switching to the new Kelvin format for the base record type, we simply
+-- need to provide another virtual field for celsius to keep everything working
+celsius :: Lens' Temperature Float
+celsius = lens getter setter
+  where
+    getter = subtract 273.15 . view kelvin
+    setter temp c = set kelvin (c + 273.15) temp
